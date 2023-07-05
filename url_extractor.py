555645754f34d90ffb2778eb2df0314504060049
Andrei <46579508+Andreisgl@users.noreply.github.com>
2023-07-05 17:03:43 -0300

    clean code

--- url_extractor.py
+++ url_extractor.py
@@ -3,26 +3,7 @@
 import os
 import urllib.request
 
-
-
-# Folder management. Delete later, maybe move to the crawler
-
-
-
-
-
-
-
 def extract_html(url):
    with urllib.request.urlopen(url) as response:
       html = response.read()
    return html
-
-
-
-
-#save_html(extract_html(search_url), data_file)
-
-
-
-pass
