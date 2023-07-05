bb5d4c11e0bbf528769103026235a4a454dfff10
Andrei <46579508+Andreisgl@users.noreply.github.com>
2023-07-05 16:39:26 -0300

    Move extractor file manager to crawler

--- crawler_longuinho.py
+++ crawler_longuinho.py
@@ -1,1 +1,37 @@
 # This file centralizes the full functionality of the crawler.
+import os
+
+import url_extractor
+import parser_longuinho
+
+def sanitize_url_to_name(input):
+   removal_list = ['http://', 'https://', 'www.']
+   
+   output = input
+   for item in removal_list:
+      output = output.replace(item, '')
+   
+   if output[-1] == '/':
+      output = output[:-1]
+
+   return output
+
+ALL_WEBSITES_FOLDER = 'SITES'
+ALL_WEBSITES_FOLDER = os.path.join('.\\', ALL_WEBSITES_FOLDER)
+
+search_url = 'http://hashomer.org.br/' # DO NOT DELETE!!! _______________________________________
+
+website_name = sanitize_url_to_name(search_url)
+
+website_folder = ''
+website_folder = os.path.join(ALL_WEBSITES_FOLDER, website_name)
+
+data_file = "data.txt"
+data_file = os.path.join(website_folder, data_file)
+
+if(not os.path.exists(ALL_WEBSITES_FOLDER)):
+   os.mkdir(ALL_WEBSITES_FOLDER)
+if(not os.path.exists(website_folder)):
+   os.mkdir(website_folder)
+
+
--- url_extractor.py
+++ url_extractor.py
@@ -3,54 +3,27 @@
 import os
 import urllib.request
 
-def sanitize_url_to_name(input):
-   removal_list = ['http://', 'https://', 'www.']
-   
-   output = input
-   for item in removal_list:
-      output = output.replace(item, '')
-   
-   if output[-1] == '/':
-      output = output[:-1]
-
-   return output
-
-ALL_WEBSITES_FOLDER = 'SITES'
-ALL_WEBSITES_FOLDER = os.path.join('.\\', ALL_WEBSITES_FOLDER)
-
-search_url = 'http://hashomer.org.br/'
-
-website_name = sanitize_url_to_name(search_url)
-
-website_folder = ''
-website_folder = os.path.join(ALL_WEBSITES_FOLDER, website_name)
 
 
-
-data_file = "data.txt"
-data_file = os.path.join(website_folder, data_file)
-
-
-if(not os.path.exists(ALL_WEBSITES_FOLDER)):
-   os.mkdir(ALL_WEBSITES_FOLDER)
-
-if(not os.path.exists(website_folder)):
-   os.mkdir(website_folder)
+# Folder management. Delete later, maybe move to the crawler
 
 
 
 
-with urllib.request.urlopen(search_url) as response:
-   html = response.read()
-
-
-with open(data_file, 'wb') as file:
-   #data = html.decode('ascii', 'ignore')
-   data = html
-   file.write(data)
 
 
 
+def extract_html(url):
+   with urllib.request.urlopen(url) as response:
+      html = response.read()
+   return html
+
+def save_html(html, filepath):
+   with open(filepath, 'wb') as file:
+      file.write(html)
+
+
+#save_html(extract_html(search_url), data_file)
 
 
 
