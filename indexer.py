15b7a34ed6b93ab6bec5aea16c52d457cf300655
Andrei <46579508+Andreisgl@users.noreply.github.com>
2023-07-05 20:17:59 -0300

    compartimentalize indexer

--- indexer.py
+++ indexer.py
@@ -1,8 +1,9 @@
-# This script sets up the links obtained in a indexed website to crawl.
+# This script saves important data from the website and returns paths for the data.
 import os
 import website_data_extractor as site_ex
 
 ALL_WEBSITES_FOLDER = 'SITES'
+
 def main_folders_manager():
     global ALL_WEBSITES_FOLDER
     ALL_WEBSITES_FOLDER = os.path.join('.\\', ALL_WEBSITES_FOLDER)
@@ -20,33 +21,34 @@ def save_list_to_file(list, path):
         file.write(aux)
         file.write(b'\n')
 
-def create_website_folder(website_name):
-    website_folder = ''
-    website_folder = os.path.join(ALL_WEBSITES_FOLDER, website_name)
+
+def website_path(name):
+    website_folder = os.path.join(ALL_WEBSITES_FOLDER, name)
     if(not os.path.exists(website_folder)):
         os.mkdir(website_folder)
-    return website_folder
+
+    data_file = 'data.txt'
+    data_file = os.path.join(website_folder, data_file)
+
+    link_list_file = 'links.txt'
+    link_list_file = os.path.join(website_folder, link_list_file)
+
+    text_list_file = 'text.txt'
+    text_list_file = os.path.join(website_folder, text_list_file)
+
+    return data_file, link_list_file, text_list_file
+
+def save_website(search_url):
+    # Saves important data from the website, returns paths for the data.
+    website_name, raw_file_data, link_list, text_list = site_ex.get_website_data(search_url)
+    data_file, link_list_file, text_list_file = website_path(website_name)
 
 
-search_url = 'http://hashomer.org.br/'
-website_name, raw_file_data, link_list, text_list = site_ex.get_website_data(search_url)
+    # Save data in folder
+    save_html_to_file(raw_file_data, data_file) # Save raw html
+    save_list_to_file(link_list, link_list_file) # Save links
+    save_list_to_file(text_list, text_list_file) # Save text
+
+    return data_file, link_list_file, text_list_file
 
 main_folders_manager()
-
-website_folder = create_website_folder(website_name)
-data_file = 'data.txt'
-data_file = os.path.join(website_folder, data_file)
-
-link_list_file = 'links.txt'
-link_list_file = os.path.join(website_folder, link_list_file)
-
-text_list_file = 'text.txt'
-text_list_file = os.path.join(website_folder, text_list_file)
-
-
-# Save data in folder
-save_html_to_file(raw_file_data, data_file) # Save raw html
-save_list_to_file(link_list, link_list_file) # Save links
-save_list_to_file(text_list, text_list_file) # Save text
-
-pass
