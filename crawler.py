b48e963daa131d7d94ae59f6ff0636891aff1feb
Andrei <46579508+Andreisgl@users.noreply.github.com>
2023-07-06 21:48:30 -0300

    rename 'link' to 'index'

--- crawler.py
+++ crawler.py
@@ -58,12 +58,12 @@ def iterate_queue(number_of_items):
     
     found_links = 0
     pages_searched = 0
-    for link in range(number_of_items):
+    for index in range(number_of_items):
         
         aux_list = []
-        list_path = indexer.get_website(incoming_link_queue[link])[1] #Possible bottleneck?
+        list_path = indexer.get_website(incoming_link_queue[index])[1] #Possible bottleneck?
 
-        print('{} of {} - {}'.format(link+1, number_of_items, incoming_link_queue[link]))
+        print('{} of {} - {}'.format(index+1, number_of_items, incoming_link_queue[index]))
 
         try:
             with open(list_path, 'r') as file:
@@ -83,7 +83,7 @@ def iterate_queue(number_of_items):
         except FileNotFoundError:
             print('FileNotFoundError EXCEPTION!')
         
-        incoming_link_queue.pop(link)
+        incoming_link_queue.pop(index)
         intermediate_link_queue = intermediate_link_queue + current_link_queue
         #incoming_link_queue = incoming_link_queue + current_link_queue
         current_link_queue.clear()
