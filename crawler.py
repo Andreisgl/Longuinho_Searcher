afd8e3d3c935bd3443df3d7fed44eb6ebe8c32bd
Andrei <46579508+Andreisgl@users.noreply.github.com>
2023-07-07 23:58:49 -0300

    fixing page counting

--- crawler.py
+++ crawler.py
@@ -268,7 +268,7 @@ def expand_index(number_of_urls_to_expand):
 
     while total_pages_searched < number_of_urls_to_expand:
         (pages_searched, number_of_items,
-            found_links) = iterate_queue(max_pages_per_iteration)
+            found_links) = iterate_queue(remaining_number_to_search)
         
         number_of_iterations += 1
         total_pages_searched += pages_searched
@@ -285,7 +285,7 @@ def expand_index(number_of_urls_to_expand):
     print('New pages found: {}'.format(total_found_links))
     pass
 
-expand_index(50)
+expand_index(10)
 
 
 
