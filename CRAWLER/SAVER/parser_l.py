220df01112e656b93fb0f9156e398994123d42fa
Andrei <46579508+Andreisgl@users.noreply.github.com>
2023-07-09 17:34:38 -0300

    optimize text_parser()

--- CRAWLER/SAVER/parser_l.py
+++ CRAWLER/SAVER/parser_l.py
@@ -50,32 +50,14 @@ def link_parser(file_data):
 
 def text_parser(file_data):
     # Return all blocks of text in the file
-    soup = BeautifulSoup(file_data, "html.parser")
+    textsoup = BeautifulSoup(file_data, "html.parser")
 
     # Split on newlines
-    text = soup.get_text()
+    text = textsoup.get_text()
     text = text.split('\n')
     #Remove '\t's
     for index in range(len(text)):
         text[index] = text[index].replace('\t', '')
+    
     #Remove empty elements from list
-    try:
-        while True:
-            text.remove('')
-    except ValueError:
-        pass    
-    return text
-
-
-
-
-
-
-
-
-#link_list = link_parser(get_file_data(test_file))
-#text_list = text_parser(get_file_data(test_file))
-
-
-
-pass
+    return [line for line in text if line != '']
