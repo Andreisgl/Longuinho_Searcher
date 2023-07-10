adfb5c5059e52c90da19556db7928e59ed102a29
Andrei <46579508+Andreisgl@users.noreply.github.com>
2023-07-10 00:31:00 -0300

    create recursive file searcher

--- indexer.py
+++ indexer.py
@@ -16,9 +16,36 @@ def main_folders_manager():
      TEXT_LIST_FILENAME,
      META_LIST_FILENAME) = site_saver.get_filenames()
 
+def file_comber(curr_dir, file_name):
+    # Recursively finds and returns paths for a file name in a directory
+    # Get proper paths for every folder in current dir
+    item_list = [os.path.join(curr_dir, x) for x in os.listdir(curr_dir)]
+    folder_list = [x for x in item_list if os.path.isdir(x)]
+    file_list = [x for x in item_list if os.path.isfile(x)]
+    path_list = []
+
+    if os.path.join(curr_dir, file_name) in file_list:
+        path_list.append(os.path.join(curr_dir, file_name))
+
+    if len(folder_list) != 0: # If there are more folders here
+        for folder in folder_list:
+            data = file_comber(folder, file_name)
+            if type(data) == list:
+                path_list += data
+            else:
+                path_list.append(data)
+    return path_list
+
+
+def get_all_of_a_file_in_database(file_name):
+    # Finds recursively all instances of a file name in the database
+    return file_comber(PAGES_DATABASE_FOLDER, file_name)
+
 
 def main():
     main_folders_manager()
+
+    auxlist = get_all_of_a_file_in_database(META_LIST_FILENAME)
     
     pass
 
