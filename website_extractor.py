85a52beb6cd48e436adb741bb377c9ae15dfd3d2
Andrei <46579508+Andreisgl@users.noreply.github.com>
2023-07-17 02:41:00 -0300

    create text and URL parsers

--- website_extractor.py
+++ website_extractor.py
@@ -1,8 +1,10 @@
-# This module extracts data from websites.
+# This module extracts data from websites. It also parses it by text and links.
 
 import os
 
 import urllib.request
+from bs4 import BeautifulSoup
+
 
 TEST_URL = 'https://en.wikipedia.org/wiki/Main_Page'
 #TEST_URL = 'https://eaa'
@@ -38,12 +40,59 @@ def page_extractor(search_url):
             error_code = e.code
 
     if success_flag:
-        data_returned = raw_data, was_redirected, final_url, http_code, success_flag
+        data_returned = (raw_data,
+                         was_redirected,
+                         final_url,
+                         http_code,
+                         success_flag)
     else:
-        data_returned = b'', was_redirected, final_url, error_code, success_flag
+        data_returned = (b'',
+                         was_redirected,
+                         final_url,
+                         error_code,
+                         success_flag)
     
     return data_returned
 
+# PARSING
+# URLs
+def parse_urls(raw_data):
+    # Returns all URLs from a page's raw data
+    url_soup = BeautifulSoup(raw_data, 'html.parser')
+    aux = url_soup.find_all('a')
+    link_list = []
+    for item in aux:
+        ref = item.get('href')
+        if ref != None and (ref.startswith('http') or ref.startswith('www')):
+            link_list.append(ref)
 
-aux = page_extractor(TEST_URL)
+    # Remove URLs containing dangerous characters
+    # Turns out some URLs really confuse Python
+    # due to them containing special characters such as '\'
+    dangerous_chars = ['\'', '\"', '\\']
+    clean_list = []
+    for link in link_list:
+        is_present = any(char in link for char in dangerous_chars)
+        if not is_present:
+            clean_list.append(link)
+
+    return clean_list
+
+# TEXT
+def parse_text(raw_data):
+    text_soup = BeautifulSoup(raw_data, 'html.parser')
+    text = text_soup.get_text()
+
+    text = text.split('\n') # Split into lines
+    text = [x.replace('\t', '') for x in text] # Remove '\t'
+    text = [x for x in text if x != ''] # Remove empty elements
+
+    return text
+
+
+data_package = page_extractor(TEST_URL)
+url_list = parse_urls(data_package[0])
+text_list = parse_text(data_package[0])
+
+
 pass
