dcc4a302b3e89a45965c80580ce684de3cba8827
Andrei <46579508+Andreisgl@users.noreply.github.com>
2025-08-22 12:36:04 -0300

    update filtering

--- website_extractor.py
+++ website_extractor.py
@@ -1,6 +1,7 @@
 # This module extracts data from websites. It also parses it by text and links.
 
 import os
+import re
 
 import urllib.request
 from bs4 import BeautifulSoup
@@ -94,7 +95,9 @@ def parse_text(raw_data):
     text = text.split('\n') # Split into lines
     text = [x.replace('\t', '') for x in text] # Remove '\t'
     text = [x for x in text if x != ''] # Remove empty elements
+    text = [re.sub('[^A-Za-z0-9]+ ', '', x) for x in text if x != ''] # Remove empty elements
 
+    
     return text
 
 # WEBSITE NAME
